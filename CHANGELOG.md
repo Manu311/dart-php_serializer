18fe794b03e535d2b879ae29d22a31ccd8f8ddea
Manuel Bihl <manu@manu311.de>
2021-03-28 16:25:30 +0200

    Documentation and Markdown-files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
-## 0.0.1
+## 0.1.0
 
-* TODO: Describe initial release.
+* Initial Release
+* Features are all available
+* Error-handling is not streamlined yet
+
--- lib/php_serializer.dart
+++ lib/php_serializer.dart
@@ -1,15 +1,29 @@
+///Converts Php-Serialized objects into Dart and vice versa
 library php_serializer;
 
 export 'serializer.dart' show phpSerialize;
-export 'deserialize.dart' show phpDeserialize;
+export 'deserializer.dart' show phpDeserialize;
 
 class InvalidSerializedString extends FormatException {
   InvalidSerializedString(String serializedString) : super(serializedString);
 }
 
+/// Contains information about complex objects which can be (de-)serialized
+///
+/// A [List] of these can be passed to [phpSerialize] and [phpDeserialize]
 class PhpSerializationObjectInformation<T extends Object> {
+  ///Full Qualified Class Name (FQCN) that has to be used in Php to identify
+  ///the class of the serialized object.
   final String serializedClassName;
-  final Object Function(Map<String, dynamic> map) objectGenerator;
+
+  ///Function which receives a [Map] of all the serialized properties and
+  ///generates the resulting object in Dart.
+  final T Function(Map<String, dynamic> map) objectGenerator;
+
+  //Object could be replaced with T, but generics inside of lambda-functions
+  //do not work (yet)
+  ///Function which receives an instance of an object and generates a [Map]
+  ///which contains all the properties of the Php-object.
   final Map<String, dynamic> Function(Object instance) dataExtractor;
 
   Type get typeOf => T;
